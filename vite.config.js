a6e90962d403d681b82f64c4e8346262b2cd65f8
LeonardoEspinoza7373 <166523336+LeonardoEspinoza7373@users.noreply.github.com>
2026-04-22 09:31:40 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      include: /src\/.*\.[jt]sx?$/,
+    })
+  ],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.jsx': 'jsx'
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
+    }
+  }
+})
